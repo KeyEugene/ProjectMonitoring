15cc4377c33e68720e524b44090c7a4178b412ce
Умников Д. <keepitok23@gmail.com>
2017-06-14 15:53:15 +0300

    Добавил пропавший Home.aspx.cs

---
+++ Teleform.ProjectMonitoring/Monitoring/Person_Page/Home.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Teleform.ProjectMonitoring.Person_Page
+{
+    public partial class Home : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
